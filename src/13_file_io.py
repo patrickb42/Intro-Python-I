f2c8eba37eaef09871f8e6b97ea879157fe09df0
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-06-08 20:05:12 -0400

    13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,16 +4,24 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+from io import DEFAULT_BUFFER_SIZE
+
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+# with open('foo.txt', 'r', buffering=DEFAULT_BUFFER_SIZE) as foo:
+with open('foo.txt', 'r', buffering=2) as foo:
+    for buffer in foo:
+      print(buffer, end = '')
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as bar:
+    bar.writelines('foo\n')
+    bar.writelines('bar\n')
+    bar.writelines('baz\n')
